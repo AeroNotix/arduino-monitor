202f66dbdc0634f7712b7f317bfddbd7420bee3a
Aaron France <aaron.l.france@gmail.com>
2020-08-18 15:46:35 +0200

    add ip/time/cpu and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# arduino-monitor
+
+Just a quick hack for using a 4 line 20 character wide LCD I2C display with an
+arduino for an at-a-glance CPU, RAM and IP readout.
+
+Displays the temperature for two CPUs because that's what I got!
+
+![the thing working](img/img.jpg)
--- main.py
+++ main.py
@@ -1,45 +1,60 @@
 import sys
 import os
 import time
+import datetime
 import serial
+import datetime
+import socket
 
-PORT='/dev/ttyUSB0'
-BAUD=115200
+PORT=os.get_env("SERIAL_PORT", "/dev/ttyUSB0")
+BAUD=int(os.get_env("SERIAL_BAUD_RATE", "115200"))
+
 if os.getenv("DEBUG", False):
-    SERIAL_CONN = sys.stdout.buffer
+  SERIAL_CONN = sys.stdout.buffer
 else:
-    SERIAL_CONN = serial.Serial(port=PORT, baudrate=BAUD)
+  SERIAL_CONN = serial.Serial(port=PORT, baudrate=BAUD)
+
+def get_ip():
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  try:
+    s.connect(('10.255.255.255', 1))
+    IP = s.getsockname()[0]
+  except Exception:
+    IP = '127.0.0.1'
+  finally:
+    s.close()
+  return IP
 
 def write(ser, output):
-    ser.write(output)
-    ser.flush()
+  ser.write(output)
+  ser.flush()
 
 def pad_line(line):
-    return line + b' ' * (20 - len(line))
+  return line + b' ' * (20 - len(line))
 
 def get_cpu_temp(n):
-    f = open('/sys/class/thermal/thermal_zone%d/temp' % n)
-    return int(f.read()) / 1000
+  f = open('/sys/class/thermal/thermal_zone%d/temp' % n)
+  return int(f.read()) / 1000
 
 def parse_memory_line(line):
-    return line.split(':')[-1].strip(' kB\n')
+  return line.split(':')[-1].strip(' kB\n')
 
 def get_mem_info():
-    f = open('/proc/meminfo')
+  with open('/proc/meminfo') as f:
     info = f.readlines()
     for line in info:
-        if 'MemTotal' in line:
-            mem_total = parse_memory_line(line)
-        if 'Active' in line:
-            mem_active = parse_memory_line(line)
+      if 'MemTotal' in line:
+        mem_total = parse_memory_line(line)
+      if 'Active:' in line:
+        mem_active = parse_memory_line(line)
     return 'Mem: {:.2f}%'.format(int(mem_active) / int(mem_total) * 100).encode()
 
 while True:
-    cpu0 = get_cpu_temp(0)
-    cpu1 = get_cpu_temp(1)
-    line0 = pad_line(b"CPU: %d\xdfc / %d\xdfc" % (cpu0, cpu1))
-    line1 = (' ' * 20).encode()
-    line2 = pad_line(get_mem_info())
-    line3 = line1
-    write(SERIAL_CONN, b''.join([line0, line1, line2, line1]))
-    time.sleep(3)
+  cpu0 = get_cpu_temp(0)
+  cpu1 = get_cpu_temp(1)
+  line0 = pad_line(b"CPU: %d\xdfc / %d\xdfc" % (cpu0, cpu1))
+  line2 = pad_line(get_mem_info())
+  line3 = pad_line("IP: {}".format(get_ip()).encode())
+  line4 = pad_line("{:>20}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')).encode())
+  write(SERIAL_CONN, b''.join([line0, line3, line2, line4]))
+  time.sleep(1)
